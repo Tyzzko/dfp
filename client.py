a5a7110599f1e6a56e59c71c6249448473051cca
Tyzzko <f.g.uporot@gmail.com>
2022-01-06 13:41:28 +0300

    5

--- client.py
+++ client.py
@@ -1,38 +1,20 @@
 import socket
-from server import IP as server_ip
 
 
-# import machine
-
-
-class Machine():  # абстрактный класс, обеспечивающий сообщение между контроллером и поворотным механизмом
+class Client:
+    '''Инициализация атрибутов класса клиента: настройка семьи адресов, максимальный
+     объем количества получаемой информации'''
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.z = 0
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.IP = socket.gethostbyname(socket.gethostname())
+        self.PORT = 11111
+        self.client.connect((self.IP, self.PORT))
+        self.data = ''
 
-    def rotate(self, x, y, z):
-        self.x += (self.x - x)
-        self.y += (self.y - y)
-        self.z += (self.z - z)
-
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # инициализация клиента
-IPs = server_ip
-PORT = 11111
-client.connect((IPs, PORT))
-rd = client.recv(1024)
-client.send("i'm OK too".encode('utf8'))
-flag = True
-rotator = Machine()
-
-while flag:  # реализация приема данных с сервера клиенту
-    data_output = ''
-    for i in range(3):
-        data = client.recv(2048)
-        data_output += data
-        if not data:
-            flag = False
-    x, y, z = [float(j) for j in data_output.split()]
-    if x or y or z:
-        rotator.rotate(x, y, z)
+    def data_reception(self):
+        '''Метод, реализующий прием данных с сервера'''
+        self.data = self.client.recv(1024).decode('utf-8')
+        if self.data:
+            pass
+        else:
+            self.client.close()
---
+++ main.py
@@ -0,0 +1,96 @@
+import socket
+from datetime import datetime
+from pyorbital.orbital import Orbital
+from requests import get
+from time import sleep  # импорт необходимых библиотек
+
+
+def return_tle():  # функция, возвращающая tle-файл
+    file = open('tle.txt', 'ab')
+    file.write(get("http://www.celestrak.com/NORAD/elements/weather.txt").content)
+    file.close()
+    return file
+
+
+class СalculationOfSpans:
+    '''Инициализация атрибутов класса спутника: координат местности, длительности рассчета пролетов,
+    времени местности по utc и создание объекта Orbital для получения данных о его местоположении'''
+
+    def __init__(self, name):
+        self.sats_name = name
+        self.sat = Orbital(self.sats_name, tle_file='tle.txt')
+        self.length = 24
+        self.lon = 43.8399
+        self.lat = 55.3949
+        self.alt = 162
+        self.utc_time = datetime.now()
+
+    def get_next_passes(self):
+        ''' Метод, возвращающий кортеж объектов datetime - времени пролетов спутника '''
+        return self.sat.get_next_passes(self.utc_time, self.lon, self.lat, self.alt, tool=0.001, horizon=0)[0]
+
+    def get_required_data(self):
+        ''' Метод-получение данных для управления поворотным мехинизмом: азимута и высоты спутника.
+        Запрашиваются посредством передачи необходимых параметров (текущего времени,
+        широты, высоты, над уровнем моря) методу get_observer_look() объекту класса Orbital библиотеки pyorbital '''
+        self.utc_time = datetime.now()
+        self.current_sat_lon, self.current_sat_lat, self.current_sat_alt = self.sat.get_lonlatalt()
+        self.azimuth, self.elevation = self.sat.get_observer_look(self.utc_time, self.current_sat_lon,
+                                                                  self.current_sat_lat, self.current_sat_alt)
+        return self.azimuth, self.elevation
+
+
+class Server:
+    '''Инициализация атрибутов класса сервера: настройка семьи адресов, количества пользователей'''
+
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.IP = socket.gethostbyname(socket.gethostname())
+        self.PORT = 11111
+        self.server.bind((self.IP, self.PORT))
+        self.server.listen(1)
+        self.connection, self.address = self.server.accept()
+        self.checkConnect = True
+
+    def data_dispatching(self, data1, data2):
+        '''Метод, раелизующий передачу данных клиенту'''
+        if self.checkConnect:
+            if not data1:
+                self.server.close()
+                self.checkConnect = False
+            else:
+                self.connection.send(f'{data1} {data2}'.encode('utf-8'))
+
+    def close(self):
+        '''Метод, закрывающий дескриптор файла сокета'''
+        self.server.close()
+
+
+if __name__ == '__main__':
+    '''Основное тело программы: пользователю предоставляется возможность выбрать интересующий его спутник, 
+    далее происходит инициализация объектов классов Server и СalculationOfSpans, запрашиивается обновление 
+    tle-файла и открывется цикл while, в котором происходят необходимые рассчеты и передача данных
+    с сервера клиенту. Завершается цикл после истечения времени пролета спутника, и тогда же закрывается сообщение
+    сервера и клиента.'''
+    sats = ['NOAA 18', 'NOAA 19', 'METEOR-M 2', 'METEOR-M2 2', 'METOP-B', 'METOP-C', 'FENGYUN 3B', 'FENGYUN 3C']
+    print(*sats, sep=', ')
+    print('Выберите спутник из списка (используйте ctrl+c - ctrl+v)')
+    name_of_sat = input()
+    while name_of_sat not in sats:
+        print('Неверно набрано имя спутника')
+        name_of_sat = input()
+
+    server = Server()  # инициализация сервера
+    sat = СalculationOfSpans(name_of_sat)
+
+    tle_file = return_tle()
+    passes = sat.get_next_passes()
+
+    if datetime.now() == passes[0]:
+        while datetime.now() != passes[-1]:
+            az, el = sat.get_required_data()
+            server.data_dispatching(f'{az} {el}')
+    else:
+        sleep(passes[0] - datetime.now())
+
+    server.close()
